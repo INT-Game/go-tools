52db2f195c484320c6f1b89c7e4be9cfe763f5d2
muyi <529757346@qq.com>
2025-02-17 16:16:07 +0800

    增加任意类型转字符串功能

--- gt_string/string.go
+++ gt_string/string.go
@@ -1,6 +1,10 @@
 package gt_string
 
-import "unicode/utf8"
+import (
+	"fmt"
+	"strconv"
+	"unicode/utf8"
+)
 
 // SubStrDecodeRuneInString 截取字符串的前 length 个字符
 func SubStrDecodeRuneInString(s string, length int) string {
@@ -10,4 +14,21 @@ func SubStrDecodeRuneInString(s string, length int) string {
 		n += size
 	}
 	return s[:n]
+}
+
+func ConvertToString(i interface{}) string {
+	switch v := i.(type) {
+	case string:
+		return v
+	case []byte:
+		return string(v)
+	case int, int32, int64:
+		return fmt.Sprintf("%d", v)
+	case float32, float64:
+		return fmt.Sprintf("%f", v)
+	case bool:
+		return strconv.FormatBool(v)
+	default:
+		return ""
+	}
 }
